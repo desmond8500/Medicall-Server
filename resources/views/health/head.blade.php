b70227329b7b07fd2ab7809b85d22dad81dfae18
Kratos <desmond8500@gmail.com>
2020-01-22 14:58:10 +0000

    test

--- resources/views/health/head.blade.php
+++ resources/views/health/head.blade.php
@@ -4,13 +4,13 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Health medical template project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="{!! asset('health/styles/bootstrap4/bootstrap.min.css') !!}">
-<link href="{!! asset('health/plugins/font-awesome-4.7.0/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="{!! asset('health/plugins/OwlCarousel2-2.2.1/owl.carousel.css') !!}">
-<link rel="stylesheet" type="text/css" href="{!! asset('health/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') !!}">
-<link rel="stylesheet" type="text/css" href="{!! asset('health/plugins/OwlCarousel2-2.2.1/animate.css') !!}">
-<link rel="stylesheet" type="text/css" href="{!! asset('health/styles/main_styles.css') !!}">
-<link rel="stylesheet" type="text/css" href="{!! asset('health/styles/responsive.css') !!}">
-<link rel="stylesheet" type="text/css" href="{!! asset('health/styles/styles.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('Medicall-Server/public/health/styles/bootstrap4/bootstrap.min.css') !!}">
+<link href="{!! asset('Medicall-Server/public/health/plugins/font-awesome-4.7.0/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="{!! asset('Medicall-Server/public/health/plugins/OwlCarousel2-2.2.1/owl.carousel.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('Medicall-Server/public/health/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('Medicall-Server/public/health/plugins/OwlCarousel2-2.2.1/animate.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('Medicall-Server/public/health/styles/main_styles.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('Medicall-Server/public/health/styles/responsive.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('Medicall-Server/public/health/styles/styles.css') !!}">
 
 </head>
