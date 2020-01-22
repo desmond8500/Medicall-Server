a031295aacad16a72ed5cca796330a31f2ec9b0d
Kratos <desmond8500@gmail.com>
2020-01-22 14:38:05 +0000

    test

--- resources/views/health/head.blade.php
+++ resources/views/health/head.blade.php
@@ -4,7 +4,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Health medical template project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="{!! asset('health/styles/bootstrap4/bootstrap.min.css') !!}">
+<link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap4/bootstrap.min.css') !!}">
+{{-- <link rel="stylesheet" type="text/css" href="{!! asset('health/styles/bootstrap4/bootstrap.min.css') !!}"> --}}
 <link rel="stylesheet" type="text/css" href="{!! asset('/home/smi/www/projects/Medicall-Server/public/health/styles/bootstrap4/bootstrap.min.css') !!}">
 <link rel="stylesheet" type="text/css" href="/home/smi/www/projects/Medicall-Server/public/health/styles/bootstrap4/bootstrap.min.css">
 <link href="{!! asset('health/plugins/font-awesome-4.7.0/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css">
