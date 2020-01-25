04ec846d788e5bbf2e444defd4ff522019d18c9f
Kratos <desmond8500@gmail.com>
2020-01-25 15:25:03 +0000

    save

--- resources/views/health/head.blade.php
+++ resources/views/health/head.blade.php
@@ -4,14 +4,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="Health medical template project">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="{!! asset('health/styles/bootstrap4/bootstrap.min.css') !!}">
-  <link rel="stylesheet" type="text/css" href="{!! asset('health/plugins/font-awesome-4.7.0/css/font-awesome.min.css') !!}" >
-  <link rel="stylesheet" type="text/css" href="{!! asset('health/plugins/OwlCarousel2-2.2.1/owl.carousel.css') !!}">
-  <link rel="stylesheet" type="text/css" href="{!! asset('health/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') !!}">
-  <link rel="stylesheet" type="text/css" href="{!! asset('health/plugins/OwlCarousel2-2.2.1/animate.css') !!}">
-  <link rel="stylesheet" type="text/css" href="{!! asset('health/styles/main_styles.css') !!}">
-  <link rel="stylesheet" type="text/css" href="{!! asset('health/styles/responsive.css') !!}">
-  <link rel="stylesheet" type="text/css" href="{!! asset('health/styles/styles.css') !!}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('health/styles/bootstrap4/bootstrap.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('health/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}" >
+  <link rel="stylesheet" type="text/css" href="{{ asset('health/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('health/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('health/plugins/OwlCarousel2-2.2.1/animate.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('health/styles/main_styles.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('health/styles/responsive.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('health/styles/styles.css') }}">
 
   <link rel="stylesheet" type="text/css" href="health/styles/bootstrap4/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="health/plugins/font-awesome-4.7.0/css/font-awesome.min.css" >
