b453a8b464fa718802a3777f9f842cc7f3addb5e
Kratos <desmond8500@gmail.com>
2020-01-25 09:25:17 +0000

    test

--- resources/views/health/head.blade.php
+++ resources/views/health/head.blade.php
@@ -4,8 +4,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Health medical template project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="{!! asset('www/health/styles/bootstrap4/bootstrap.min.css') !!}">
-<link rel="stylesheet" type="text/css" href="www/health/styles/bootstrap4/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="public/health/styles/bootstrap4/bootstrap.min.js">
+<link rel="stylesheet" type="text/css" href="/www/projets/Medicall-Server/public/health/styles/bootstrap4/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="www/projets/Medicall-Server/public/health/styles/bootstrap4/bootstrap.min.css">
+
+
 
 <link rel="stylesheet" type="text/css" href="{!! asset('health/styles/bootstrap4/bootstrap.min.css') !!}">
 {{-- <link href="{!! asset('health/plugins/font-awesome-4.7.0/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css">
