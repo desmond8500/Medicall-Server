0554d831e390cb323ba8d1fc62b029f9e19d4d1b
Kratos <desmond8500@gmail.com>
2020-01-25 23:16:39 +0000

    update css

--- resources/views/health/head.blade.php
+++ resources/views/health/head.blade.php
@@ -13,6 +13,15 @@
   <link rel="stylesheet" type="text/css" href="{{ asset('health/styles/responsive.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ asset('health/styles/styles.css') }}">
 
+  <link rel="stylesheet" type="text/css" href="{{ asset('Medicall-Server/public/health/styles/bootstrap4/bootstrap.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('Medicall-Server/public/health/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}" >
+  <link rel="stylesheet" type="text/css" href="{{ asset('Medicall-Server/public/health/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('Medicall-Server/public/health/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('Medicall-Server/public/health/plugins/OwlCarousel2-2.2.1/animate.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('Medicall-Server/public/health/styles/main_styles.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('Medicall-Server/public/health/styles/responsive.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('Medicall-Server/public/health/styles/styles.css') }}">
+
   <link rel="stylesheet" type="text/css" href="health/styles/bootstrap4/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="health/plugins/font-awesome-4.7.0/css/font-awesome.min.css" >
   <link rel="stylesheet" type="text/css" href="health/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
