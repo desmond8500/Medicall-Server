e73574cfee7c37a6c88de1779aa1a13a4c059f23
Kratos <desmond8500@gmail.com>
2020-03-02 10:28:59 +0000

    update

--- resources/views/health/landing/content/calltoaction.blade.php
+++ resources/views/health/landing/content/calltoaction.blade.php
@@ -22,11 +22,30 @@
         </div>
         <div class="col-md-4">
             <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <div class="card-header">
+                    Prendre un rendez-vous
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <form action="/rendezvous" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label></label>
+                            <select name="" class="form-control">
+                                <option value="Infirmier">Infirmier</option>
+                                <option value="Sage-femme">Sage-femme</option>
+                                <option value="Kinésithérapeute">Kinésithérapeute</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Nom</label>
+                            <input type="text" class="form-control" name="nom">
+                        </div>
+                        <div class="form-group">
+                            <label>Téléphone</label>
+                            <input type="text" class="form-control" name="tel">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Valider</button>
+                    </form>
                 </div>
             </div>
         </div>
