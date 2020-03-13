e25080965b9f35ec59480dacae090edb985e1ce4
Desmond Miles <desmond8500@gmail.com>
2020-03-13 14:31:20 +0000

    navbar update

--- resources/views/health/landing/index/navbar.blade.php
+++ resources/views/health/landing/index/navbar.blade.php
@@ -15,33 +15,38 @@
          +221 77 837 23 90
         </p>
     </div>
-    <div class=" d-none d-sm-block col-md-9 menu">
-        <div class="row">
-            @foreach ($menu as $item)
-            <div class="col-md-1">
-                <a href="{!! route($item['route']) !!}" class="submenu">{{ strtoupper($item['name']) }}</a>
-            </div>
-            @endforeach
+</div>
+
+{{-- Menu  --}}
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand text-bold" href="{{ route('health') }}">Accueil</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
             @isset($user)
                 @if ($user->role == 'admin')
-                <div class="col-md-1">
-                    <a href="{!! route('admin') !!}" class="submenu">ADMINISTRATION</a>
-                </div>
+                    <li class="nav-item active">
+                        <a href="{!! route('admin') !!}" class="submenu">ADMINISTRATION</a>
+                    </li>
                 @endif
             @endisset
-        </div>
-    </div>
-    <div class="col-xs-6 col-md-3 menu text-right">
-        <div class="container">
+        </ul>
+
+
+        <div class="form-inline my-2 my-lg-0">
             @isset($user)
                 <a href="{{ route('quit')}}" class="btn btn-light color-primary">Déconnexion</a>
             @else
-                <a href="{{ route('healthlogin')}}" class="btn btn-light color-primary">Connexion</a>
-                <a href="{{ route('healthregister')}} " class="btn btn-outline-light">Inscription</a>
+                <a href="{{ route('healthlogin')}}" class="btn btn-light color-primary m-2">Connexion</a>
+                <a href="{{ route('healthregister')}} " class="btn btn-outline-light m-2">Inscription</a>
             @endisset
         </div>
     </div>
-</div>
-{{-- menu  --}}
+</nav>
 
 
