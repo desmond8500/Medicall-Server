7722c81010cda065e7dd21ff0eb0147b58f32eef
Kratos <desmond8500@gmail.com>
2020-05-12 08:42:12 +0000

    table update

--- resources/views/0 vacation/pages/admin-newsletter.blade.php
+++ resources/views/0 vacation/pages/admin-newsletter.blade.php
@@ -18,29 +18,31 @@
 
 @section('content')
 <section class="container">
-    <table class="table table-bordered">
-        <thead class="thead-dark">
-            <tr>
-                <th class="text-center">#</th>
-                <th> Email</th>
-                {{-- <th>Edit</th> --}}
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data as $key => $user)
-            <tr>
-                <td class="text-center">{{ $key + 1 }}</td>
-                <td>{{ $user->email }}</td>
-                {{-- <td>
-                    @include('0 vacation.modal.editUser')
-                    <a href="{{route('admin.user.delete',['id'=> $user->id])}} " class="btn btn-danger">
-                        <i class="fa fa-trash"></i>
-                    </a>
-                </td> --}}
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th class="text-center">#</th>
+                    <th> Email</th>
+                    {{-- <th>Edit</th> --}}
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data as $key => $user)
+                <tr>
+                    <td class="text-center">{{ $key + 1 }}</td>
+                    <td>{{ $user->email }}</td>
+                    {{-- <td>
+                        @include('0 vacation.modal.editUser')
+                        <a href="{{route('admin.user.delete',['id'=> $user->id])}} " class="btn btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </a>
+                    </td> --}}
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </section>
 @endsection
 
