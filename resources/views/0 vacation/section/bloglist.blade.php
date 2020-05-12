c4ff06318ca78c40494028aa50a92d37e6c946c4
Kratos <desmond8500@gmail.com>
2020-05-12 09:29:05 +0000

    save

--- resources/views/0 vacation/section/bloglist.blade.php
+++ resources/views/0 vacation/section/bloglist.blade.php
@@ -9,20 +9,22 @@
         <div class="row d-flex">
             @foreach ($posts as $post)
                 <div class="col-md-4 ftco-animate">
-                    <div class="blog-entry align-self-stretch">
+                    <div class="blog-entry align-self-stretch card">
                         {{-- <a href="{{ route('blogpage',['id'=>$post->id])}}" class="block-20 rounded" style="background-image: url('{{ $post->featured_image }}');">
                         </a> --}}
                         <a href="{{ route('blogpage',['id'=>$post->id])}}" class="text-center">
                             <img src="{{ $post->featured_image }}" alt="{{ $post->featured_image }}" height="150px" class="rounded mx-auto d-block" >
                         </a>
-                        <div class="text p-4 text-center">
-                        <h3 class="heading"><a href="{{ route('blogpage',['id'=>$post->id])}}">{{ $post->title }}</a></h3>
-                        <div class="meta mb-2">
-                            {{-- <div><a href="#">{{ $post->date }}</a></div> --}}
-                            {{-- <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div> --}}
-                        </div>
-                        <p>{{ $post->summary }}</p>
+                        <div class="text p-4 ">
+                            <h3 class="heading text-center">
+                                <a href="{{ route('blogpage',['id'=>$post->id])}}">{{ $post->title }}</a>
+                            </h3>
+                            {{-- <div class="meta mb-2">
+                                <div><a href="#">{{ $post->date }}</a></div>
+                                <div><a href="#">Admin</a></div>
+                                <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
+                            </div> --}}
+                            <p>{{ $post->summary }}</p>
                         </div>
                     </div>
                 </div>
