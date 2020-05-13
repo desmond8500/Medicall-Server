216c415b0afe836e94d10edc42c1a17160a65c61
Kratos <desmond8500@gmail.com>
2020-05-13 16:00:53 +0000

    ggh

--- resources/views/0 vacation/index/wrap.blade.php
+++ resources/views/0 vacation/index/wrap.blade.php
@@ -15,11 +15,11 @@
                         @yield('adminmenu')
 
                         @auth
-                            @if ($user->role=='admin')
+                            {{-- @if ($user->role=='admin')
                                 <a href="{{route('admin.index')}}" class="d-flex align-items-center justify-content-center">
                                     <span class="fa fa-user"><i class="sr-only">Facebook</i></span>
                                 </a>
-                            @endif
+                            @endif --}}
                         @endauth
 
 
